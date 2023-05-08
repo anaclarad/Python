773dcab3649992384254eb7282d8f7bc01587784
anaclarad <anaclaradjo813@gmail.com>
2023-05-08 15:24:45 -0300

    first commit

---
+++ Tópicos1/Exemplos.py
@@ -0,0 +1,23 @@
+#saída de dados
+print('Ola, pessoa')
+#exemplo de como estaria as aspas dulpas sem um fechamento
+print('Explícito', 'é', 'melhor " do que implícito')
+#exemplo de caractere de escape
+print("Minha avó \" está doida")
+#exemplo de separador 
+nome="Ana Clara"
+idade=18
+print(nome,idade, sep='-')
+print("Ana clara", 20 ,sep=', ')
+#exemplo de end
+print("Feira", end='. \n 1')
+#segue abixo DocString
+"""
+    Esse é o DocString
+"""
+#tipo, int,float, string, boolean suscessivamente
+print(type(20))
+print(type(20.5))
+print(type('Ana'))
+print(type(True))
+
---
+++ Tópicos1/exer_1.py
@@ -0,0 +1,12 @@
+nome= 'Ana Clara'
+sobrenome= 'de Jesus Oliveira'
+idade = 20
+ano_nascimento=2002
+altura=160
+
+if idade > 18:
+    print("maior de idade")
+else:
+    print("menor de idade")
+
+
